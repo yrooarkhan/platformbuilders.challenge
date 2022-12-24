70a901be21af73ce5c79e6a9eb01876617273312
felipe.cipriani <felipe.cipriani@softplan.com.br>
2022-12-24 16:03:55 -0300

    Criação do projeto (first commit)

---
+++ src/test/java/io/platformbuilders/challenge/infrastructure/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.platformbuilders.challenge.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
